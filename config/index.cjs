72a983e903f9ba9574683ed1aa30519643c764af
cuixunlong <84705637+cuixunlong@users.noreply.github.com>
2023-10-26 18:09:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oHPP76RtiDhx2A7RjZAxUqg7Q_iU',
+      id: 'oHPP76UNzLP69X7pbB_fGKOZIJpc',
     }
   ],
 
